7da4b1d030cf325fe2ec000e7ac41d6e4382b14f
Isaac Mayowa OJO <mayorjo4ever@gmail.com>
2026-02-21 14:00:08 +0100

    start server update
    start server update

--- start-server.bat
+++ start-server.bat
@@ -4,18 +4,17 @@ setlocal
 :: Default IP address
 set "local_ip=127.0.0.1"
 
-:: Try to get the first non-loopback IPv4 address
+:: Try to get first non-loopback IPv4
 for /f "tokens=2 delims=:" %%f in ('ipconfig ^| findstr /i "IPv4" ^| findstr /v "127.0.0.1"') do (
     set "local_ip=%%f"
     goto after_loop
 )
 
 :after_loop
-:: Clean up whitespace
 setlocal enabledelayedexpansion
 set "local_ip=!local_ip: =!"
 
-:: Set the port
+:: Set port
 set "port=8001"
 
 echo.
@@ -23,13 +22,14 @@ echo Using IP: %local_ip%
 echo Opening Application at: http://%local_ip%:%port%
 echo.
 
+
 :: Open the app in your default browser
 start http://%local_ip%:%port%
 
-:: Change directory to your Laravel project
+:: Change to project directory
 cd /d "c:\xampp\htdocs\bloodlinks"
 
-:: Start PHP server
-php -S --host=%local_ip% --port=%port%
+:: Start PHP built-in server
+php -S %local_ip%:%port%
 
 pause
